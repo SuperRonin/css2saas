e9a6ea1dd6f496381a65e1062de3f8a33e0c411d
xuexingwei <xuexingwei@fenxiangbuy.com>
2022-03-27 00:01:09 +0800

    feat: cmd params dev~

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,17 @@
+#!/usr/bin/env node
 const { program } = require('commander');
-const { extractFiles } = require('./io.js')
+const { extractFiles } = require('../lib/io.js')
 
 program
-  .name('css2saas')
-  .description('A css2saas tool')
-  .version('0.0.1');
+  .name('css-pipe-all')
+  .description('A css-pipe tool')
+  .version('1.0.12');
 
 program.command('run')
   .description('begin run')
   .argument('<string>', 'A dirName or fileName')
+  .requiredOption('-t, --type <type>', 'what do you want to tanslate file ?')
+  .option('-opt, --settings [settings...]', 'specify settings')
   .option('--tab2', 'will be 2 tab')
   .option('--tab4', 'will be 4 tab')
   .option('--del-out', 'will delete outside comments')
@@ -16,6 +19,7 @@ program.command('run')
   .action((str, options) => {
     console.log('options', options);
     console.log(str)
+    extractFiles(str, 'wxss', options.settings)
   });
-
+// if(!program.addArgument) console.log('Error: Please write dirname !')
 program.parse(process.argv)
