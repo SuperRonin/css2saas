86bfde9ce1e13d258428c4e11222cad0b8669aa8
xuexingwei <xuexingwei@fenxiangbuy.com>
2022-03-28 00:17:13 +0800

    feat: 基本功能完成开发

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { program } = require('commander');
-const { extractFiles } = require('../lib/io.js')
+const { extractFiles, delFiles } = require('../lib/io.js')
 
 program
   .name('css-pipe-all')
@@ -16,9 +16,14 @@ program.command('run')
   .option('--tab4', 'will be 4 tab')
   .option('--del-out', 'will delete outside comments')
   .option('--del-in', 'will delete in comments')
+  .option('--del-file', 'will delete args type file')
   .action((str, options) => {
     console.log('options', options);
     console.log(str)
+    if(options.settings.includes('delFile') && options.type){
+      delFiles(str, options.type)
+      return
+    }
     extractFiles(str, 'wxss', options.settings)
   });
 // if(!program.addArgument) console.log('Error: Please write dirname !')
--- test/index.js
+++ test/index.js
@@ -1,17 +1,47 @@
 const fs = require('fs');
 const path = require('path');
-const minifiers = require('css-minifiers');
-const csso = minifiers.csso;
+var CleanCSS = require('clean-css');
+const { extractFiles } = require('../lib/io.js')
 
-const dirName = '/index.wxss'; //要转换的文件路径
-fs.readFile(path.join(__dirname, dirName), 'utf-8', function (err, data) {
-  if (err) {
-    console.error(err)
-    return
-  }
+// const dirName = '/index.wxss'; //要转换的文件路径
+// fs.readFile(path.join(__dirname, dirName), 'utf-8', function (err, data) {
+//   if (err) {
+//     console.error(err)
+//     return
+//   }
 
-  csso(data).then(function (output) {
-    // 当前目录生成编译后的scss文件
-    fs.writeFileSync(path.join(__dirname, 'index.scss'), parseCSS(output))
-  })
-})
+//   data = data.replace(/\/\*/g, '/*!')
+
+//   new CleanCSS({ returnPromise: true })
+//   .minify(data)
+//   .then(output => {
+//     console.log('output', output);
+//   })
+// })
+delFile('test', 'wxss')
+function delFile(dir, suffix) {
+  fs.readdir(path.resolve(dir), (err, files) => {
+    err && console.warn('路径读取出错', err);
+    //遍历读取到的文件列表
+    files.forEach((filename) => {
+      let filedir = path.join(dir, filename);//完整的路径
+
+      //根据文件路径获取文件信息，返回一个fs.Stats对象
+      fs.stat(filedir, (error, stats) => {
+        error && console.warn('获取文件stats失败', error);
+        let isFile = stats.isFile(); //是文件
+        let isDir = stats.isDirectory(); //是文件夹
+        if (isFile) {
+          if (filename.split('.').pop().toLowerCase() === suffix) {
+            fs.unlinkSync(filedir);
+          }
+        }
+        isDir && ergodicDir(filedir, format); //递归，如果是文件夹，就继续遍历该文件夹下面的文件
+      })
+    });
+  });
+}
+
+
+
+
